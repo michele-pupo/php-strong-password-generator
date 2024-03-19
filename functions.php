99e5289e0d8f6220d6bf8755220a7a692ba18f14
michele-pupo <mikypupo@hotmail.it>
2024-03-19 16:22:45 +0100

    added additional parameters for password generation

--- functions.php
+++ functions.php
@@ -1,14 +1,33 @@
 <?php
     // funzione per generare una password casuale
-    function generaPassword($lunghezza) {
-        // caratteri possibili della password da generare
-        $caratteri = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-_=+[]{}|;:,.<>?';
+    function generaPassword($lunghezza, $usa_numeri = false, $usa_lettere_maiuscole = false, $usa_lettere_minuscole = false, $usa_simboli = false, $ripetizione_caratteri = false) {
+        // possibili caratteri per la password in base ai parametri forniti
+        $caratteri = '';
+        if ($usa_numeri) {
+            $caratteri .= '0123456789';
+        }
+        if ($usa_lettere_minuscole) {
+            $caratteri .= 'abcdefghijklmnopqrstuvwxyz';
+        }
+        if ($usa_lettere_maiuscole) {
+            $caratteri .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        }
+        if ($usa_simboli) {
+            $caratteri .= '!@#$%^&*()-_=+[]{}|;:,.<>?';
+        }
+
+        // lunghezza della stringa di caratteri
         $lunghezza_caratteri = strlen($caratteri);
         $password = '';
 
-        // generiamo una password casuale utilizzando caratteri casuali
+        // generazione della password utilizzando caratteri casuali
         for ($i = 0; $i < $lunghezza; $i++) {
-            $password .= $caratteri[rand(0, $lunghezza_caratteri - 1)];
+            // se la ripetizione dei caratteri non è consentita tramite checkbox, non inseriamo lo stesso carattere 2 volte
+            do {
+                $carattere_scelto = $caratteri[rand(0, $lunghezza_caratteri - 1)];
+            } while (!$ripetizione_caratteri && $i > 0 && $carattere_scelto === $password[$i - 1]);
+
+            $password .= $carattere_scelto;
         }
 
         return $password;
--- index.php
+++ index.php
@@ -8,29 +8,65 @@
     <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
-<body>
+<body data-bs-theme="dark">
     <div class="container">
-        <h1>PHP Strong Password Generator</h1>
+        <h1>Generatore di password</h1>
 
         <form action="index.php" method="get">
             <label for="lunghezza">Lunghezza della Password:</label>
-            <input type="number" id="lunghezza" name="lunghezza" min="8" required>
+
+            <!-- indica la lunghezza minima della password -->
+            <input type="number" id="lunghezza" name="lunghezza" min="8">
+
+            <!-- indica l'uso di numeri nella password -->
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="usa_numeri" name="usa_numeri">
+                <label class="form-check-label" for="usa_numeri">Usa numeri</label>
+            </div>
+            <!-- indica l'uso di lettere maiuscole nella password -->
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="usa_lettere_maiuscole" name="usa_lettere_maiuscole">
+                <label class="form-check-label" for="usa_lettere_maiuscole">Usa lettere maiuscole</label>
+            </div>
+
+            <!-- indica l'uso di lettere minuscole nella password -->
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="usa_lettere_minuscole" name="usa_lettere_minuscole">
+                <label class="form-check-label" for="usa_lettere_minuscole">Usa lettere minuscole</label>
+            </div>
+
+            <!-- indica l'uso di simboli nella password -->
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="usa_simboli" name="usa_simboli">
+                <label class="form-check-label" for="usa_simboli">Usa simboli</label>
+            </div>
+
+            <!-- indica il poter ripetere caratteri già usati nella password-->
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="ripetizione_caratteri" name="ripetizione_caratteri">
+                <label class="form-check-label" for="ripetizione_caratteri">Permetti ripetizione di caratteri uguali</label>
+            </div>
             <button type="submit">Genera Password</button>
         </form>
     </div>
 
     <div class="container">
         <?php 
-        
+
             // includiamo il file functions
             include 'functions.php';
             
             // controllo sulla richiesta GET per la lunghezza della password
             if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['lunghezza'])) {
                 $lunghezza = $_GET['lunghezza'];
+                $usa_numeri = isset($_GET['usa_numeri']);
+                $usa_lettere_maiuscole = isset($_GET['usa_lettere_maiuscole']);
+                $usa_lettere_minuscole = isset($_GET['usa_lettere_minuscole']);
+                $usa_simboli = isset($_GET['usa_simboli']);
+                $ripetizione_caratteri = isset($_GET['ripetizione_caratteri']);
 
                 // visualizziamo la password casuale dopo averla generata
-                $password_generata = generaPassword($lunghezza);
+                $password_generata = generaPassword($lunghezza, $usa_numeri, $usa_lettere_maiuscole, $usa_lettere_minuscole, $usa_simboli, $ripetizione_caratteri);
                 echo "<h2>Password Casuale Generata:</h2>";
                 echo "<p>$password_generata</p>";
             }
